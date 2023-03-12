413fc83924b009c40db9c13731126de327229123
Salman Younas <98895288+Salman7243@users.noreply.github.com>
2023-03-13 02:36:15 +0400

    Add files via upload
    about today

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:tiktok_app/screen/home_screen.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter tik tok',
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
+    );
+  }
+}
